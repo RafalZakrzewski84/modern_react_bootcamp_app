4030e2f4d14b2772b626b8d4987502bb48f97659
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-06-30 15:13:45 +0200

    adding files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Car from './Car';
+import Home from './Home';
+
 function App() {
 	return (
 		<div className="App">
-			<h1>4_30_EXERCISE: Fruits Modules Solution - import, export </h1>
+			<h1>31. Create React App Conventions</h1>
+			<Car />
+			<Home />
 		</div>
 	);
 }
---
+++ src/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+	return <div>I am Home from Home.js</div>;
+}
+
+export default Home;
